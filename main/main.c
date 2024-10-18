d258997438c97611c00edc352482aff7ee84c37a
Flint2082 <125222178+Flint2082@users.noreply.github.com>
2024-10-18 19:51:11 +0200

    adc handler

--- main/main.c
+++ main/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_adc/adc_continuous.h"
+#include "esp_mac.h"
+
 
 void app_main(void)
 {
+    adc_continuous_handle_t handle;
+    adc_continuous_handle_cfg_t adc_config = {
+        .max_store_buf_size = 1024,
+        .conv_frame_size = 100,
+    };
+    // ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config));
 
 }
