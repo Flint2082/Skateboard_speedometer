8f2c041f89f0fb508989aacfbca45d28126570d4
Flint2082 <125222178+Flint2082@users.noreply.github.com>
2024-10-28 19:53:58 +0100

    bugfixes

--- main/adc.h
+++ main/adc.h
@@ -1,19 +1,36 @@
 #ifndef ADC_H
 #define ADC_H
 
-#define EXAMPLE_ADC_UNIT                    ADC_UNIT_1
-#define _EXAMPLE_ADC_UNIT_STR(unit)         #unit
-#define EXAMPLE_ADC_UNIT_STR(unit)          _EXAMPLE_ADC_UNIT_STR(unit)
-#define EXAMPLE_ADC_CONV_MODE               ADC_CONV_SINGLE_UNIT_1
-#define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_0
-#define EXAMPLE_ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
+#include <string.h>
+#include <stdio.h>
 
-#define EXAMPLE_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
-#define EXAMPLE_ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
-#define EXAMPLE_ADC_GET_DATA(p_data)        ((p_data)->type1.data)
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_adc/adc_continuous.h"
+#include "esp_mac.h"
 
-#define EXAMPLE_READ_LEN                    256
 
+
+#define ADC_UNIT                    ADC_UNIT_1
+#define _ADC_UNIT_STR(unit)         #unit
+#define ADC_UNIT_STR(unit)          _ADC_UNIT_STR(unit)
+#define ADC_CONV_MODE               ADC_CONV_SINGLE_UNIT_1
+#define ADC_ATTEN                   ADC_ATTEN_DB_0
+#define ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
+
+#define ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
+#define ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
+#define ADC_GET_DATA(p_data)        ((p_data)->type1.data)
+
+#define READ_LEN                    256
+
+
+
+
+static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle); 
 
 
 #endif // ADC_H
--- main/main.c
+++ main/main.c
@@ -1,38 +1,81 @@
 #include "main.h"
+#include "adc.h"
 
 
+static adc_channel_t channel[2] = {ADC_CHANNEL_6, ADC_CHANNEL_7};
 
+static TaskHandle_t s_task_handle;
+static const char *TAG = "EXAMPLE"; 
+
+static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
+{
+    BaseType_t mustYield = pdFALSE;
+    //Notify that ADC continuous driver has done enough number of conversions
+    vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
+
+    return (mustYield == pdTRUE);
+}
 
 void app_main(void)
 {
-    
-    // adc_continuous_handle_cfg_t adc_init_config = {
-    //     .max_store_buf_size = 1024,
-    //     .conv_frame_size = 100,
-    // };
-    // // ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_init_config));
-    // adc_continuous_config_t adc_config = {
-    //     .pattern_num = 1,
-    //     // .adc_pattern = ?,
-    //     .sample_freq_hz = 10,                                                   // low for now - was 1000
-    //     .conv_mode = ADC_CONV_SINGLE_UNIT_1,
-    //     .format = ADC_DIGI_OUTPUT_FORMAT_TYPE1
-    // };
+    esp_err_t ret;
+    uint32_t ret_num = 0;
+    uint8_t result[READ_LEN] = {0};
+    memset(result, 0xcc, READ_LEN);
 
-    // adc_digi_pattern_config_t adc_digi_config = {
-    //     .atten = ADC_ATTEN_DB_0,
-    //     .channel = ADC_CHANNEL_0, // is this the correct channel?
-    //     .unit = ADC_UNIT_1, // what does this mean?
-    //     .bit_width = ADC_BITWIDTH_10
-    // };
+    s_task_handle = xTaskGetCurrentTaskHandle();
 
-    // adc_continuous_config();
+    adc_continuous_handle_t handle = NULL;
+    continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t), &handle);
 
-    while(true)
-    {
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    //     printf("ADC: %i", ADC_val); // print ADC value
+    adc_continuous_evt_cbs_t cbs = {
+        .on_conv_done = s_conv_done_cb,
+    };
+    ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
+    ESP_ERROR_CHECK(adc_continuous_start(handle));
+
+    while (1) {
+
+        /**
+         * This is to show you the way to use the ADC continuous mode driver event callback.
+         * This `ulTaskNotifyTake` will block when the data processing in the task is fast.
+         * However in this example, the data processing (print) is slow, so you barely block here.
+         *
+         * Without using this event callback (to notify this task), you can still just call
+         * `adc_continuous_read()` here in a loop, with/without a certain block timeout.
+         */
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        char unit[] = ADC_UNIT_STR(ADC_UNIT);
+
+        while (1) {
+            ret = adc_continuous_read(handle, result, READ_LEN, &ret_num, 0);
+            if (ret == ESP_OK) {
+                ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
+                for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
+                    adc_digi_output_data_t *p = (adc_digi_output_data_t*)&result[i];
+                    uint32_t chan_num = ADC_GET_CHANNEL(p);
+                    uint32_t data = ADC_GET_DATA(p);
+                    /* Check the channel number validation, the data is invalid if the channel num exceed the maximum channel */
+                    if (chan_num < SOC_ADC_CHANNEL_NUM(ADC_UNIT)) {
+                        ESP_LOGI(TAG, "Unit: %s, Channel: %"PRIu32", Value: %"PRIx32, unit, chan_num, data);
+                    } else {
+                        ESP_LOGW(TAG, "Invalid data [%s_%"PRIu32"_%"PRIx32"]", unit, chan_num, data);
+                    }
+                }
+                /**
+                 * Because printing is slow, so every time you call `ulTaskNotifyTake`, it will immediately return.
+                 * To avoid a task watchdog timeout, add a delay here. When you replace the way you process the data,
+                 * usually you don't need this delay (as this task will block for a while).
+                 */
+                vTaskDelay(1);
+            } else if (ret == ESP_ERR_TIMEOUT) {
+                //We try to read `READ_LEN` until API returns timeout, which means there's no available data
+                break;
+            }
+        }
     }
-    // ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config));
 
+    ESP_ERROR_CHECK(adc_continuous_stop(handle));
+    ESP_ERROR_CHECK(adc_continuous_deinit(handle));
 }
